7def374e9842d5c8772aa3d5f605a53990ed0b5b
Dima <125818471+DmitriyKislyakov@users.noreply.github.com>
2024-09-09 08:21:07 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 const myLibrary = []
 
-function Book(title, author, read = false) {
+function Book(title, author, read) {
   this.title = title
   this.author = author
-  this.read = false
+  this.read = read
 }
 
 function isBook(book) {
@@ -14,9 +14,39 @@ function addBookToLibrary(book) {
   if (isBook(book)) myLibrary.push(book)
 }
 
-addBookToLibrary('HP')
-const harryPotter = new Book('Harry Potter', 'Rouling', true)
+function addBookCard(book) {
+  const cards = document.querySelector('.cards')
+
+  const card = document.createElement('div')
+  card.classList.add('card')
+  cards.appendChild(card)
+
+  const title = document.createElement('h3')
+  title.textContent = book.title
+  card.appendChild(title)
+
+  const author = document.createElement('p')
+  author.textContent = `Writing by ${book.author}`
+  card.appendChild(author)
+
+  const read = document.createElement('p')
+  read.textContent = `Read: ${book.read}`
+  card.appendChild(read)
+}
+
+const harryPotter = new Book('Harry Potter', 'Rouling', 'yes')
 
 addBookToLibrary(harryPotter)
 
-console.log(myLibrary)
+const darkTower = new Book('Dark Tower', 'King', 'yes')
+
+addBookToLibrary(darkTower)
+
+const hobbit = new Book('Hobbit', 'Tolkien', 'no')
+
+myLibrary.forEach((book) => addBookCard(book))
+
+//addBookCard(harryPotter)
+
+//console.log(harryPotter.read)
+//const cards = document.querySelectorAll('.card')
